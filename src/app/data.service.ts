6b56f7d5d8ce3f242c0f453a3377247736be385b
Kanzaki404 <alexander.risteski23@gmail.com>
2020-03-28 01:59:32 +0100

    some Progress

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,15 +1,45 @@
 export class DataService{
-    articles:object[] = [{      
-         title:'',
-        author:'',
-        content:'',
-        timeToRead: 0}
+    articles:object[] = [];
     
-    ];
-
+    // createArticle(article:object){
+    //     console.log(this.articles.length)
+    //     this.articles.push(article)
+    //     for(let i = 0; i<this.articles.length; i++){
+    //         let key:string = i.toString();
+    //         let art:string = JSON.stringify(this.articles[i]);
+    //         localStorage.setItem(key,art);
+    //         let toStr = this.articles.length -1;
+    //         localStorage.setItem('last', toStr.toString())
+    //     }
+    // }
+    key:number = 0;
     createArticle(article:object){
-        this.articles.push(article)
+        
+        
+        if(localStorage.getItem('last') === null){
 
-        console.log(this.articles)
+            localStorage.setItem(this.key.toString(),JSON.stringify(article))
+            localStorage.setItem('last', this.key.toString())
+
+        }else{
+            this.key = parseInt(localStorage.getItem('last')); 
+            this.key++
+            localStorage.setItem(this.key.toString(),JSON.stringify(article))  
+            localStorage.setItem('last', this.key.toString())
+        }
+       
+    }
+
+    getAllArticles(){
+        let num = parseInt(localStorage.getItem('last'));
+        for(let i=0; i<num; i++){
+            this.articles.push(JSON.parse(localStorage.getItem(i.toString())))
+        }
+        return this.articles
+    }
+
+    getLastArticle(){
+        let key = localStorage.getItem('last')
+        console.log(localStorage.getItem(key))
     }
 }
--- src/app/remove-article/remove-article.component.ts
+++ src/app/remove-article/remove-article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-remove-article',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./remove-article.component.css']
 })
 export class RemoveArticleComponent implements OnInit {
-  articles = ['bismark1','bismark2','bismark23','bismark4','bismark5']
-  constructor() { }
+  articles:object[] = [];
+  constructor(private data:DataService) {}
 
   ngOnInit(): void {
   }
+
+  getArticles(){
+
+    if(this.articles.length === 0 && this.articles.length < this.data.getAllArticles().length ){
+      this.articles = this.data.getAllArticles()
+    }else{
+      alert('nothing to fetch')
+    }
+    
+  }
+
 
 }
